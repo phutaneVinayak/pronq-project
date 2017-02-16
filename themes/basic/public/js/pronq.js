ecb3002cc5e2576ab1a606f34f3b0157fd330a4e
Vinayak Phutane <vinayak.phutane@raweng.com>
2017-02-16 14:59:45 +0530

    new changes

--- themes/basic/public/js/pronq.js
+++ themes/basic/public/js/pronq.js
@@ -1,19 +1,9 @@
 
 $(document).ready(function(){
     $('#partner2').mouseover(function(){
-    		// $('.media').hide();
-    		// $('.media').find('.tringle').removeClass().addClass('tringle2');
-    		// $('.rp_feedback').show();
-    		// $('.media').show();
-    		$(".new").hide();
-    		$('.rp_feedback').show();
+    		$('.media').find('.tringle').removeClass().addClass('tringle2');
+    		
     }).mouseout(function(){
-    		// $('.media').hide();
-    		// $('.media').find('.tringle2').removeClass().addClass('tringle');
-    		// $('.media').find('.media-body').removeClass().addClass('rp_feedback');
-    		// $('.media').find('.avnet_feedback').removeClass().addClass('media-body');
-    		// $('.media').show();
-    		$(".new").show();
-    		$('.rp_feedback').hide();
+    		$('.media').find('.tringle2').removeClass().addClass('tringle');
     });
 });
