1bed8c80383d3243701f9bad91cff30921b033b6
Vinayak Phutane <vinayak.phutane@raweng.com>
2017-02-16 14:24:09 +0530

    Added changes for pronq

--- themes/basic/public/js/pronq.js
+++ themes/basic/public/js/pronq.js
@@ -1,7 +1,19 @@
+
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip({
-    										animated:'fade',
-    										placement:'bottom',
-    										html:true,
-    									});
+    $('#partner2').mouseover(function(){
+    		// $('.media').hide();
+    		// $('.media').find('.tringle').removeClass().addClass('tringle2');
+    		// $('.rp_feedback').show();
+    		// $('.media').show();
+    		$(".new").hide();
+    		$('.rp_feedback').show();
+    }).mouseout(function(){
+    		// $('.media').hide();
+    		// $('.media').find('.tringle2').removeClass().addClass('tringle');
+    		// $('.media').find('.media-body').removeClass().addClass('rp_feedback');
+    		// $('.media').find('.avnet_feedback').removeClass().addClass('media-body');
+    		// $('.media').show();
+    		$(".new").show();
+    		$('.rp_feedback').hide();
+    });
 });
