c6fdd77c23362ea72d46cd87bd2e3f22b91e3b7d
Vinayak Phutane <vinayak.phutane@raweng.com>
2017-02-20 10:17:02 +0530

    modified css

--- themes/basic/public/js/pronq.js
+++ themes/basic/public/js/pronq.js
@@ -6,4 +6,20 @@ $(document).ready(function(){
     }).mouseout(function(){
     		$('.media').find('.tringle2').removeClass().addClass('tringle');
     });
+    var temp;
+    data = "<div Class='hover_cover'> <p class=''>"+ newdata +"</p> <a 		href='#' onclick='return false' class='close_link' style='text-decoration:none;'> Close </a> </div>"
+    $('.cantainer-fluid .middle_container .tools .tool_box .learn_more a').click(function(evt){
+    	evt.preventDefault();
+    	var test = $(this).parent();
+    	var realParent=test.parent();
+    	temp = realParent.html();
+    	realParent.html(data);
+    });
+
+    $(document).on('click', '.tool_box a.close_link', function(evt){
+    	evt.preventDefault();
+		var spaceholder = $(this).parent().parent()
+		spaceholder.html(temp);
+    	evt.preventDefault();
+	});
 });
